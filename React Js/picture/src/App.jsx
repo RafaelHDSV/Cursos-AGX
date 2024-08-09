9937a3a7c29cc8b21dc82df37240c139475485e8
Rafael Vieira <rafaelvieira1720@gmail.com>
2024-08-09 14:48:15 -0300

    feat: adding pictures project

---
+++ React Js/picture/src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css';
+
+export default function App() {
+	const picture = document.querySelector('.picture');
+	const background = document.querySelector('.background');
+
+	function backgroundClass() {
+		background.classList.toggle('background--active');
+	}
+
+	function pictureClass() {
+		picture.classList.toggle('picture--active');
+	}
+
+	return (
+		<div
+			className='background background--active'
+			onClick={backgroundClass}
+		>
+			<img
+				className='picture'
+				src='https://i.imgur.com/5qwVYb1.jpeg'
+				alt='Casas de arco-íris em Kampung Pelangi, Indonésia'
+				onClick={pictureClass}
+			/>
+
+			<span>Hello</span>
+		</div>
+	);
+}
