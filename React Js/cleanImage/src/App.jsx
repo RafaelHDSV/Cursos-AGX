89a02dba9b0e1c744cc1b4d8d04e61470164ea03
Rafael Vieira <rafaelvieira1720@gmail.com>
2024-08-09 16:56:45 -0300

    feat: adding cleanImage

--- React Js/cleanImage/src/App.jsx
+++ React Js/cleanImage/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 
+import './App.css';
+
 export default function Gallery() {
 	const [index, setIndex] = useState(0);
 	const hasNext = index < images.length - 1;
@@ -19,7 +21,9 @@ export default function Gallery() {
 			<h3>
 				Image {index + 1} of {images.length}
 			</h3>
+
 			<img key={image.src} src={image.src} />
+
 			<p>{image.place}</p>
 		</>
 	);
