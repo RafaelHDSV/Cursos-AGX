4568c2e5e8a4d5aaba95f530e3261ac30c8184fc
Rafael Vieira <rafaelvieira1720@gmail.com>
2024-08-13 14:32:32 -0300

    feat: updating example

--- React Js/Documentação/example.jsx
+++ React Js/Documentação/example.jsx
@@ -1,49 +1,20 @@
 import { useState, useEffect } from 'react';
-import { experimental_useEffectEvent as useEffectEvent } from 'react';
 
 export default function Timer() {
 	const [count, setCount] = useState(0);
-	const [increment, setIncrement] = useState(1);
-
-	const onTick = useEffectEvent(() => {
-		setCount((c) => c + increment);
-	});
 
 	useEffect(() => {
+		console.log('✅ Creating an interval');
+
 		const id = setInterval(() => {
-			onTick();
+			console.log('⏰ Interval tick');
+			setCount((c) => c + 1);
 		}, 1000);
 		return () => {
+			console.log('❌ Clearing an interval');
 			clearInterval(id);
 		};
 	}, []);
 
-	return (
-		<>
-			<h1>
-				Counter: {count}
-				<button onClick={() => setCount(0)}>Reset</button>
-			</h1>
-			<hr />
-			<p>
-				Every second, increment by:
-				<button
-					disabled={increment === 0}
-					onClick={() => {
-						setIncrement((i) => i - 1);
-					}}
-				>
-					–
-				</button>
-				<b>{increment}</b>
-				<button
-					onClick={() => {
-						setIncrement((i) => i + 1);
-					}}
-				>
-					+
-				</button>
-			</p>
-		</>
-	);
+	return <h1>Counter: {count}</h1>;
 }
